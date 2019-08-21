e9574cfdd09e55727bec4410e01a73821e386861
Daniel Saidi <daniel.saidi@gmail.com>
2019-08-22 00:42:32 +0200

    Add new icon to demo app

---
+++ Mockery/Mockery.h
@@ -0,0 +1,19 @@
+//
+//  Mockery.h
+//  Mockery
+//
+//  Created by Daniel Saidi on 2019-08-21.
+//  Copyright © 2019 Daniel Saidi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Mockery.
+FOUNDATION_EXPORT double MockeryVersionNumber;
+
+//! Project version string for Mockery.
+FOUNDATION_EXPORT const unsigned char MockeryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mockery/PublicHeader.h>
+
+
