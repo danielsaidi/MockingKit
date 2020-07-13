55a8bf47b065616e085b6326db54cbee89e6801b
Daniel Saidi <daniel.saidi@gmail.com>
2020-07-13 13:14:53 +0200

    Add failing test for Xcode 12 bug

--- Tests/MockeryTests/Mockable+MemoryAddressTests.swift
+++ Tests/MockeryTests/Mockable+MemoryAddressTests.swift
@@ -14,15 +14,27 @@ class Mockable_MemoryAddressTests: QuickSpec {
         
         describe("address of function") {
             
-            it("is different for different signatures") {
-                let add1 = obj.address(of: obj.handleArg1)
-                let add2 = obj.address(of: obj.handleArg2)
+            it("is same for same function") {
+                let add1 = obj.address(of: obj.handle)
+                let add2 = obj.address(of: obj.handle)
+                expect(add1).to(equal(add2))
+            }
+            
+            it("is different for function with different name") {
+                let add1 = obj.address(of: obj.handle)
+                let add2 = obj.address(of: obj.handle2)
                 expect(add1).toNot(equal(add2))
             }
             
-            it("is different for same signature") {
-                let add1 = obj.address(of: obj.handleArg1)
-                let add2 = obj.address(of: obj.handleArg1_1)
+            it("is different for function with different argument type") {
+                let add1 = obj.address(of: obj.handle)
+                let add2 = obj.address(of: obj.handle3)
+                expect(add1).toNot(equal(add2))
+            }
+            
+            it("is different for function with different return type") {
+                let add1 = obj.address(of: obj.handle)
+                let add2 = obj.address(of: obj.handle4)
                 expect(add1).toNot(equal(add2))
             }
         }
@@ -31,12 +43,14 @@ class Mockable_MemoryAddressTests: QuickSpec {
 
 private class TestClass: Mock {
     
-    struct ArgType1 {}
-    struct ArgType2 {}
+    struct Arg1 {}
+    struct Arg2 {}
     struct ReturnType1 {}
     struct ReturnType2 {}
+    struct ReturnType3 {}
     
-    func handleArg1(_ obj: ArgType1) -> ReturnType1? { nil }
-    func handleArg1_1(_ obj: ArgType1) -> ReturnType1? { nil }
-    func handleArg2(_ obj: ArgType2) -> ReturnType2? { nil }
+    func handle(_ obj: Arg1) -> ReturnType1? { nil }
+    func handle2(_ obj: Arg1) -> ReturnType1? { nil }
+    func handle3(_ obj: Arg2) -> ReturnType2? { nil }
+    func handle4(_ obj: Arg1) -> ReturnType3? { nil }
 }
