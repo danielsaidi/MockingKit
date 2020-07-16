62ae32c85c01288b4adeeebf9263b7a241b1a70a
Daniel Saidi <daniel.saidi@gmail.com>
2020-07-16 17:09:32 +0200

    Deprecate Mockable+MemoryAddress

--- Sources/Mockery/Deprecations.swift
+++ Sources/Mockery/Deprecations.swift
@@ -9,6 +9,14 @@ import Foundation
 
 public extension Mockable {
     
+    @available(*, deprecated, message: "Use MockReference instead")
+    func address<Arguments, Result>(of function: @escaping (Arguments) throws -> Result) -> MemoryAddress {
+        let (_, lo) = unsafeBitCast(function, to: (Int, Int).self)
+        let offset = MemoryLayout<Int>.size == 8 ? 16 : 12
+        let pointer = UnsafePointer<Int>(bitPattern: lo + offset)!
+        return pointer.pointee
+    }
+    
     @available(*, deprecated, renamed: "invokations(of:)")
     func executions<Arguments, Result>(of function: @escaping (Arguments) throws -> Result) -> [Invokation<Arguments, Result>] {
         invokations(of: function)
@@ -16,4 +24,4 @@ public extension Mockable {
 }
 
 @available(*, deprecated, message: "Use MockReference instead")
-typealias MemoryAddress = Int
+public typealias MemoryAddress = Int
