c6657acec8bea4685aaa5660226a03d008ac0bd2
Daniel Saidi <daniel.saidi@gmail.com>
2020-07-16 17:05:48 +0200

    Add mock reference, which will replace the need for a memory address

---
+++ Sources/Mockery/MockReference.swift
@@ -0,0 +1,23 @@
+//
+//  MockReference.swift
+//  Mockery
+//
+//  Created by Daniel Saidi on 2020-07-16.
+//  Copyright © 2020 Daniel Saidi. All rights reserved.
+//
+
+import Foundation
+
+/**
+ This struct is used to get a unique reference to a function.
+ */
+public struct MockReference<Arguments, Result>: Identifiable {
+    
+    public init(function: @escaping (Arguments) throws -> Result) {
+        self.id = UUID()
+        self.function = function
+    }
+    
+    public let id: UUID
+    public let function: (Arguments) throws -> Result
+}
