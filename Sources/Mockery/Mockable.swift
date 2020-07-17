e948dc5b578fae2066f337d0590c3acd3ffd47c2
Daniel Saidi <daniel.saidi@gmail.com>
2020-07-17 08:47:34 +0200

    Update the docs for Mockable

--- Sources/Mockery/Mockable.swift
+++ Sources/Mockery/Mockable.swift
@@ -9,52 +9,66 @@
 import Foundation
 
 /**
- This protocol represents any mockable test class and can be
- used to record function calls and return any pre-registered
+ This protocol can be implemented by any mock that should be
+ able to record function calls and return any pre-registered
  return values.
  
- When you implement this protocol, you must provide a `mock`.
- If a mock does not have to inherit another class, it should
- inherit `Mock`, since it implements `Mockable` by providing
- itself as the mock:
+ To implement this protocol, you must just provide the class
+ with a `mock`. If the class doesn't have to inherit another
+ class, you can just inherit the `Mock` class instead, which
+ implements `Mockable` by using itself as `mock`.
+ 
+ Let's look at how easy it is to create a mock of a protocol.
+ Let's say that we have a `Printer` that can print a text:
  
  ```swift
  protocol Printer {
-    func print(_ text: String)
- }
- 
- // Inherit `Mock` if you don't have to inherit a base class
- class MockPrinter: Mock, Printer {
-    func print(_ text: String) {
-        invoke(print, arguments: (text))
-    }
- }
- 
- // Implement `Mockable` if you have to inherit a base class
- class MockPrinter: BasePrinter, Mockable {
-    let mock = Mock()
-    func print(_ text: String) {
-        invoke(print, arguments: (text))
-    }
+     func print(_ text: String)
  }
  ```
+ 
+ The easiest way to create a mocked `Printer` is to create a
+ class that inherits `Mock` and implements `Printer`:
+ 
+ ```swift
+ class MockPrinter: Mock, Printer {
+ 
+     lazy var invokeRef = MockReference(invoke)  // Must be lazy
+ 
+     func print(_ text: String) {
+         invoke(invokeRef, arguments: (text))
+     }
+ }
+ ```
+ 
+ Note the lazy reference. It's needed to keep track of how a
+ mock is used. It must also be lazy so the signature becomes
+ correct. Your mock now just have to call `invoke` to record
+ the call and return any pre-registered return value, if any.
+ 
+ To use this mock, you just have to create an instance of it
+ and use it like you would use any other implementation. You
+ can inject it into any class that depends on a `Printer` to
+ verify that the class does what it should.
 
- Calling `invoke` records functions calls so you can inspect
- them later:
+ When the mock calls `invoke`, it basically records the call
+ so that you can inspect it later:
  
  ```swift
  let printer = MockPrinter()
  printer.print("Hello!")
- let invokations = printer.invokations(of: printer.print)   // => 1 item
- invokations[0].arguments.0                                 // => "Hello!"
- printer.hasInvoked(printer.print)                          // => true
- printer.hasInvoked(printer.print, numberOfTimes: 1)        // => true
- printer.hasInvoked(printer.print, numberOfTimes: 2)        // => false
+ let inv = printer.invokations(of: printer.printRef)    // => 1 item
+ inv[0].arguments.0                                     // => "Hello!"
+ printer.hasInvoked(printer.printRef)                   // => true
+ printer.hasInvoked(printer.printRef, numberOfTimes: 1) // => true
+ printer.hasInvoked(printer.printRef, numberOfTimes: 2) // => false
  ```
  
- You can use `invoke` in the same way for void functions and
- for functions that return a result. For returning functions,
- `invoke` will return any pre-registered return value:
+ Note that you call the function, but inspect the reference.
+ 
+ If your mocked function returns a value, you must `register`
+ a return value before calling it. `invoke` will then return
+ the pre-registered value:
  
  ```swift
  protocol Converter {
@@ -62,31 +76,35 @@ import Foundation
  }
  
  class MockConverter: Mock, Converter {
+ 
+     lazy var convertRef = MockReference(convert)
+ 
      func convert(_ text: String) -> String {
-         invoke(convert, arguments: (text))
+         invoke(convertRef, arguments: (text))
      }
  }
  ```
  
  Make sure to register non-optional results before calling a
- returning function, or it will crash when it's being called:
+ returning function, otherwise your test will crash:
  
  ```swift
  let converter = MockStringConverter()
- let result = converter.convert("banana")                   // => Crash!
+ let result = converter.convert("banana") // => Crash!
  converter.registerResult(for: convert) { input in input.reversed() }
- let result = converter.convert("banana")                   // => Returns "ananab"
+ let result = converter.convert("banana") // => Returns "ananab"
  ```
  
- Since the result block is called with the original argument
- collection, you can vary the result based on the arguments.
+ You don't have to do this for optional return values, since
+ not registering a value will just make them return `nil`.
+ 
+ Note that the result block receives the arguments that were
+ used to call the function. This means that you can vary the
+ return value depending on the input arguments.
  
  `TODO` For now, this protocol has no error registration for
  functions that do not return a value. This means that async
  functions can't register custom completion errors.
- 
- `TODO` The function address approach doesn't work when your
- mock class is defined in another target than the test.
 */
 public protocol Mockable {
     
