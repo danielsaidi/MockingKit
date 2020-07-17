40f41827b760616a82f4ceefe4f01a6de82ee0bd
Daniel Saidi <daniel.saidi@gmail.com>
2020-07-17 11:46:38 +0200

    Add more tests

---
+++ Tests/MockeryTests/GenericTests.swift
@@ -0,0 +1,37 @@
+//
+//  MockableTests.swift
+//  MockeryTests
+//
+//  Created by Daniel Saidi on 2020-07-17.
+//  Copyright © 2020 Daniel Saidi. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import Mockery
+
+class GenericTests: QuickSpec {
+    
+    override func spec() {
+        
+        describe("generic mocks") {
+            
+            it("should work") {
+                let mock = GenericMock<Int>()
+                mock.doit(with: 42)
+                let inv = mock.invokations(of: mock.doitRef)
+                expect(inv.count).to(equal(1))
+                expect(inv[0].arguments).to(equal(42))
+            }
+        }
+    }
+}
+
+private class GenericMock<T>: Mock {
+    
+    lazy var doitRef = MockReference(doit)
+    
+    func doit(with value: T) {
+        invoke(doitRef, args: (value))
+    }
+}
--- Tests/MockeryTests/MockableTests.swift
+++ Tests/MockeryTests/MockableTests.swift
@@ -14,10 +14,10 @@ class MockableTests: QuickSpec {
 
     override func spec() {
         
-        var mock: TestMockable!
+        var mock: TestClass!
 
         beforeEach {
-            mock = TestMockable()
+            mock = TestClass()
         }
         
         describe("registering result") {
@@ -248,5 +248,63 @@ class MockableTests: QuickSpec {
                 expect(mock.hasInvoked(mock.functionWithStringResultRef)).to(beTrue())
             }
         }
+    }
+}
+
+private class TestClass: TestProtocol, Mockable {
+    
+    var mock = Mock()
+    
+    lazy var functionWithIntResultRef = MockReference(functionWithIntResult)
+    lazy var functionWithStringResultRef = MockReference(functionWithStringResult)
+    lazy var functionWithStructResultRef = MockReference(functionWithStructResult)
+    lazy var functionWithClassResultRef = MockReference(functionWithClassResult)
+    lazy var functionWithOptionalIntResultRef = MockReference(functionWithOptionalIntResult)
+    lazy var functionWithOptionalStringResultRef = MockReference(functionWithOptionalStringResult)
+    lazy var functionWithOptionalStructResultRef = MockReference(functionWithOptionalStructResult)
+    lazy var functionWithOptionalClassResultRef = MockReference(functionWithOptionalClassResult)
+    lazy var functionWithVoidResultRef = MockReference(functionWithVoidResult)
+    lazy var asyncFunctionRef = MockReference(asyncFunction)
+    
+    func functionWithIntResult(arg1: String, arg2: Int) -> Int {
+        invoke(functionWithIntResultRef, args: (arg1, arg2))
+    }
+    
+    func functionWithStringResult(arg1: String, arg2: Int) -> String {
+        invoke(functionWithStringResultRef, args: (arg1, arg2))
+    }
+
+    func functionWithStructResult(arg1: String, arg2: Int) -> User {
+        invoke(functionWithStructResultRef, args: (arg1, arg2))
+    }
+
+    func functionWithClassResult(arg1: String, arg2: Int) -> Thing {
+        invoke(functionWithClassResultRef, args: (arg1, arg2))
+    }
+
+
+    func functionWithOptionalIntResult(arg1: String, arg2: Int) -> Int? {
+        invoke(functionWithOptionalIntResultRef, args: (arg1, arg2))
+    }
+
+    func functionWithOptionalStringResult(arg1: String, arg2: Int) -> String? {
+        invoke(functionWithOptionalStringResultRef, args: (arg1, arg2))
+    }
+
+    func functionWithOptionalStructResult(arg1: String, arg2: Int) -> User? {
+        invoke(functionWithOptionalStructResultRef, args: (arg1, arg2))
+    }
+
+    func functionWithOptionalClassResult(arg1: String, arg2: Int) -> Thing? {
+        invoke(functionWithOptionalClassResultRef, args: (arg1, arg2))
+    }
+
+
+    func functionWithVoidResult(arg1: String, arg2: Int) {
+        invoke(functionWithVoidResultRef, args: (arg1, arg2))
+    }
+
+    func asyncFunction(arg1: String, completion: @escaping (Error?) -> Void) {
+        invoke(asyncFunctionRef, args: escaping(arg1, completion))
     }
 }
