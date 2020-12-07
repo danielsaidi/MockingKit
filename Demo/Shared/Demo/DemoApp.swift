a4bc3dc08a5fdcc12ec55c8864bc415dc8063317
Daniel Saidi <daniel.saidi@gmail.com>
2020-12-07 22:36:31 +0100

    Begin adding a new, universal demo

---
+++ Demo/Shared/Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Shared
+//
+//  Created by Daniel Saidi on 2020-12-07.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Demo/Shared/Resources/Image+Demo.swift
@@ -0,0 +1,39 @@
+//
+//  Image+Demo.swift
+//  Demo
+//
+//  Created by Daniel Saidi on 2020-11-26.
+//  Copyright © 2020 Daniel Saidi. All rights reserved.
+//
+
+import SwiftUI
+
+extension Image {
+    
+    static var alert: Image { Image(systemName: "rectangle.center.inset.fill") }
+    static var authentication: Image { Image(systemName: "faceid") }
+    static var base64: Image { Image(systemName: "hryvniasign.square") }
+    static var bundle: Image { Image(systemName: "app.gift") }
+    static var circle: Image { Image(systemName: "circle") }
+    static var circleFilled: Image { Image(systemName: "circle.fill") }
+    static var clear: Image { Image(systemName: "xmark.circle") }
+    static var cloud: Image { Image(systemName: "cloud") }
+    static var data: Image { Image(systemName: "cylinder.split.1x2") }
+    static var date: Image { Image(systemName: "calendar") }
+    static var device: Image { Image(systemName: "apps.iphone") }
+    static var dismiss: Image { Image(systemName: "xmark") }
+    static var extensions: Image { Image(systemName: "puzzlepiece") }
+    static var file: Image { Image(systemName: "doc") }
+    static var fileAdd: Image { Image(systemName: "doc.badge.plus") }
+    static var fileSearch: Image { Image(systemName: "doc.text.magnifyingglass") }
+    static var files: Image { Image(systemName: "doc.on.doc") }
+    static var flag: Image { Image(systemName: "flag") }
+    static var folder: Image { Image(systemName: "folder") }
+    static var globe: Image { Image(systemName: "globe") }
+    static var ioc: Image { Image(systemName: "shippingbox") }
+    static var key: Image { Image(systemName: "key") }
+    static var map: Image { Image(systemName: "map") }
+    static var pin: Image { Image(systemName: "mappin") }
+    static var service: Image { Image(systemName: "gearshape") }
+    static var trash: Image { Image(systemName: "trash") }
+}
